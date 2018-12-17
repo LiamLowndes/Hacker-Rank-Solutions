47f778aee78516ea14e099de562ad27fece31a7e
LiamLowndes <36050320+LiamLowndes@users.noreply.github.com>
2018-12-17 10:58:22 -0500

    Create Answer.py

---
+++ Compress the String/Answer.py
@@ -0,0 +1,29 @@
+text = input()
+
+ans = []
+loop = 0
+
+while (len(text) > 0):
+    
+    count = 1
+    num = int(text[0])
+
+    while(True):
+        
+        if (count) > (len(text)-1):
+            break
+        
+        if num == int(text[count]):
+            count += 1
+        else:
+            break
+    
+    ans.append((count, num))
+    
+    for b in range(count):
+        text = text[1:]
+        
+    loop += 1
+    
+for a in range(len(ans)):
+    print (ans[a], end=" ")
